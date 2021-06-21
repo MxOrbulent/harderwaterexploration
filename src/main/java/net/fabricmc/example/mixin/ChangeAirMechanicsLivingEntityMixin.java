875fdfc2634fd884c5c3006e395cd0b0687ce58b
MxOrbulent <devops@mxorbulent.space>
2021-06-21 16:03:34 +0100

    Added dependency on cloth config in preperation for making things configureable. Made air drain twice as fast (a balance act between max air and air drain ticks are neccesary to achivie the air time you think someone should have)

--- src/main/java/net/fabricmc/example/mixin/ChangeAirMechanicsLivingEntityMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ChangeAirMechanicsLivingEntityMixin.java
@@ -7,15 +7,49 @@ import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class ChangeAirMechanicsLivingEntityMixin {
-	@Inject(method = "getNextAirUnderwater", at = @At("RETURN"), cancellable = true)
-private void changemaxair(CallbackInfoReturnable<Integer> value) {
-  value.setReturnValue(100);
+  /* abstract class LivingEntityMixin {
+    @Redirect(method = "getNextAirUnderwater",
+              at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;"))
+    private Integer changereturn(int configureable) {
+        return i > 0 && this.random.nextInt(i + 1) > 0 ? air : air - configureable;
+    }
+} */
+
+
+
+
+
+
+
+  	@Inject(method = "getNextAirUnderwater()I", at = @At("RETURN"), cancellable = true)
+private void changereturnintovariable(CallbackInfoReturnable<Integer> value) {
+  int additionaldrainticks = 1;
+  int airbeforeadditionaldrain = value.getReturnValue();
+  int airafteradditionaldrain = airbeforeadditionaldrain - additionaldrainticks;
+  //System.out.println("[AIR BEFORE DRAIN]: " + airbeforeadditionaldrain);
+  //System.out.println("[AIR AFTER DRAIN]: " + airafteradditionaldrain);
+  if (airafteradditionaldrain < -20) {
+    airafteradditionaldrain = -20;
+  }
+  if (airbeforeadditionaldrain <= 0) {
+    value.setReturnValue(airbeforeadditionaldrain);
+  } else {
+    value.setReturnValue(airafteradditionaldrain);
+  }
+  
 }
+
+/*
+@Inject(method = "getNextAirUnderwater", at = @At("TAIL"), cancellable = true)
+private void changemethod(CallbackInfoReturnable info) {
+  int newair = i > 0 && this.random.nextInt(i + 1) > 0 ? air : air - 2;
+} */
 
 
 
